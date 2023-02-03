bc67bdc3ae3cf36497f398349ad8f39bfd9d3586
JevonCW <jevcwatson@gmail.com>
2023-02-03 19:58:04 +0800

    placeholder project demos text

--- src/components/ProjectsModal.vue
+++ src/components/ProjectsModal.vue
@@ -2,6 +2,9 @@
     <div class="modal-overlay">
       <div class="modal">
         <h6>Project Demos</h6>
+        <hr>
+        <h6>Coming Soon</h6>
+        <hr>
         <button @click="$emit('close-modal')">Close</button>
       </div>
     </div>
